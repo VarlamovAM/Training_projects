a405116cce718357ccd919108da148f30d447b3b
Varlamov.AM <varlamovantonij@gmail.com>
2020-06-06 15:57:06 +0300

    New version:
    Added new function to count colour coefficients.

--- Simulation_of_gravity/Colour_Maker.py
+++ Simulation_of_gravity/Colour_Maker.py
@@ -1,11 +1,10 @@
-def Make_colour_index(Meaning, minimum, maximum):
+import math
 
-    Red_index_colour = int((Meaning - minimum)/(maximum - minimum) * (255 - 60)) + 60
+
+def Make_colour_index(Meaning, minimum, maximum):
+    Red_index_colour = 60 + int((580 * math.atan((Meaning - minimum) / (maximum - minimum))) / math.pi)
     index = hex(Red_index_colour)
     index = index.lstrip('0x')
-    results = '#' + index  + '0000'
-
+    results = '#' + index + '0000'
 
     return results
-
-
