9f30ddaa51571571bf090a7c183770e43e6fb720
Varlamov.AM <varlamovantonij@gmail.com>
2020-06-06 15:56:07 +0300

    New version:
    In this version in programm adde test to inputs data from User.

--- Simulation_of_gravity/main_GUI.py
+++ Simulation_of_gravity/main_GUI.py
@@ -3,32 +3,56 @@ import Search as Search
 import Import_Data as ID
 import Export_Data as ED
 import Colour_Maker as CM
+import Check_Input as CI
 import os
 
 
 def main():
+    global a, minimum, maximum
     width_of_window = int(1005)
     height_of_window = int(1005)
 
+    points = []
+    correct_input = True
 
-    Points = []
-    N = int(input("Введите число точек: (Целое число, после ввода нажмите enter)"))
+    while correct_input:
+        N = input("Введите число точек: (Целое число, после ввода нажмите enter)")
 
+        if not N.isdigit():
+            print('Некорректный ввод, повторите попытку: ')
+            continue
 
-    for i in range(N):
-        Points.append([input("Введите массу точки в десятичном формате (XXX.X), затем нажмите enter)"),
-                       input("Введите координату x точки (целое число), затем нажмите enter"),
-                       input("Введите координату н точки (целое число), затем нажмите enter")])
+        N = int(N)
 
+        if N == 0:
+            print('Введите число, отличное от 0')
+            continue
 
-    ED.Export_Data(Points, N)
+        i = 0
+        while i < N:
+            points.append([input("Введите массу точки в десятичном формате (XXX.X), затем нажмите enter): "),
+                           input("Введите координату x точки (целое число, не больше 200), затем нажмите enter: "),
+                           input("Введите координату н точки (целое число, не больше 200), затем нажмите enter: ")])
 
-    os.system('.\Simulation.exe')
+            if ((CI.is_digit(points[i][0]) is False or (points[i][0].isdigit())) or
+                    ((points[i][1].isdigit() is False) or (int(points[i][1]) < 0 or int(points[i][1]) > 200)) or
+                    ((points[i][2].isdigit() is False) or (int(points[i][2]) < 0 or int(points[i][2]) > 200))):
+                print('Вы ввели некоректные данные. Повторите ввод данный последней точки: ')
+                points.pop(i)
+            else:
+                i = i + 1
 
-    Data, a = ID.Import_data("Data.txt")
-    maximum, minimum = Search.Search(Data, a)
+        ED.Export_Data(points, N)
 
+        os.system('.\Simulation.exe')
 
+        Data, a = ID.Import_data("Data.txt")
+        maximum, minimum = Search.Search(Data, a)
+
+        os.system('del Data.txt')
+        os.system('del Points.txt')
+
+        correct_input = False
 
     root = Tk()
     c = Canvas(root, width=width_of_window, height=height_of_window, bg='white')
